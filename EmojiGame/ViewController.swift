b011cb3e047e4b1529c7d6ddd88a6b31a57fe52b
KarVarr <megatr9n@gmail.com>
2022-11-11 17:17:42 +0300

    added some buttons and text

--- EmojiGame/ViewController.swift
+++ EmojiGame/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+   
+    @IBOutlet weak var textFields: UILabel!
+    
+
+    
+    @IBAction func buttonAction(_ sender: UIButton) {
+        
+        
     }
-
-
+    
+    
 }
 
